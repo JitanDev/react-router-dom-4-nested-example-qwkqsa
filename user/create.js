8588f5fd9d57f0fb1ca363be85bd607fc141906e
JitanDev <jitan.developer@gmail.com>
2019-07-23 17:24:52 +0530

    1 file modified

--- user/create.js
+++ user/create.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class UserCreate extends React.Component {
     render() {
         
         return (
             <div>
+            <Link to="/user">Back</Link>
                <h1>form design wil go here</h1>
             </div>
         );
